4e37a272f54392f238b8da2aed3328ffa864fbe5
Finn Else-McCormick <2200864@uad.ac.uk>
2025-01-22 15:00:42 +0000

    Setup C# project, added script for automatically downloading godot executable

---
+++ godot-install/setup_godot_win_x86_64.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Downloading Godot v4.3 .NET...
+powershell -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest https://github.com/godotengine/godot/releases/download/4.3-stable/Godot_v4.3-stable_mono_win64.zip -OutFile Godot.zip"
+echo Unzipping...
+powershell -Command "$ProgressPreference = 'SilentlyContinue'; Expand-Archive Godot.zip"
+move /Y Godot\Godot_v4.3-stable_mono_win64\GodotSharp .
+move /Y Godot\Godot_v4.3-stable_mono_win64\Godot_v4.3-stable_mono_win64.exe .
+rmdir /S /Q "Godot"
+del "Godot.zip"
+ren Godot_v4.3-stable_mono_win64.exe Godot.exe
