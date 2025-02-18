dc90bd944f8b7e1563936656fcaf464a3d7f4b92
beaparsons <2202436@uad.ac.uk>
2025-02-18 15:58:59 +0000

    continued work on deckbuilding

--- scenes/Topic1.cs
+++ scenes/Topic1.cs
@@ -4,27 +4,35 @@ using System;
 public partial class Topic1 : Sprite2D
 {
 	private bool selected =false;
-	public int ID = 1;
-	public int deckspot;
+	private bool over = false;
+	public string ID = "card1";
 	// Called when the node enters the scene tree for the first time.
-[Signal] public delegate void L1ADDEventHandler(int ID);
+[Signal] public delegate void L1ADDEventHandler(string ID);
+[Signal] public delegate void L1REMOVEEventHandler(string ID);
 
 	public override void _Ready()
 	{
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		//if selected gray out
-	}
-
-	public void select() 
-	{
-		if(Input.IsActionPressed("Left_Click")){
+		if(Input.IsActionPressed("Left_Click") && over){
 		selected = !selected;
-		//if selected =true, add card id to decklist save deck spot
-		//else remove from decklist and replace deckspot with another card
-		}
+		if(selected){
+			EmitSignal(SignalName.L1ADD, ID);
+		}else{
+			EmitSignal(SignalName.L1REMOVE, ID);
+		}}
+		//if selected gray out
+}	
+
+	public void _mouseover() 
+	{
+		over = true;
 	} 
+	public void _mouseleave(){
+		over = false;
+	}
 }
--- scenes/TopicDecklist.cs
+++ scenes/TopicDecklist.cs
@@ -4,7 +4,7 @@ using System;
 public partial class TopicDecklist : Label
 {
 	// Called when the node enters the scene tree for the first time.
-	private int[] decklist = {0,0,0,0,0,0};
+	private string[] decklist = {" 1"," 2"," 3"," 4"," 5"," 6"};
 	private int cardplace = 0;
 
 	public override void _Ready()
@@ -14,10 +14,38 @@ public partial class TopicDecklist : Label
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		string full =" ";
+
+		for(int i =0; i <6; i++){
+			full = full + decklist[i];
+		}
+		
+		Text = full;
+
 	}
 
-	private void deckadd(int id) {
+	
+	private void deckadd(string id) {
+		if(cardplace > 5){
+			cardplace = 5;
+		}
 		decklist[cardplace] = id;
 		 cardplace++;
+	}
+
+	private void deckremove(string id){
+		bool found= false;
+		for(int i =0; i <cardplace; i++){
+			if(found){
+				decklist[i-1]= decklist[i];
+			}
+			if(decklist[i] == id){
+				found = true;
+			}
+		}
+		if(found){
+			decklist[cardplace] = " ";
+			cardplace--;
+		}
 	}
 }
