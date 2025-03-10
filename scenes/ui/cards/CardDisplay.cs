1d3e710969cb3ba1c054d9bd940e29e4fff92b35
Finn Else-McCormick <2200864@uad.ac.uk>
2025-03-10 19:27:35 +0000

    Fix CardSharedParams not being properly loaded in the editor

--- scenes/ui/cards/CardDisplay.cs
+++ scenes/ui/cards/CardDisplay.cs
@@ -51,19 +51,13 @@ public partial class CardDisplay : Control, IReloadableToolScript
 	public Texture2D Texture { get; private set { field = value; RebuildMeshes(); UpdatePivot(); (GetParent() as Container)?.QueueSort(); } }
 	private static readonly GradientTexture1D s_shadowGradientTexture;
 	
-	private static readonly CardSharedParams SharedParams;
-
 	static CardDisplay() {
 		var gradient = new Gradient{ InterpolationColorSpace = Gradient.ColorSpace.Oklab };
 		gradient.SetColor(0, Colors.Black); gradient.SetColor(1, new Color(Colors.Black, 0f));
 		s_shadowGradientTexture = new GradientTexture1D { Gradient = gradient };
-
-		if (!Engine.IsEditorHint()) { SharedParams = ResourceLoader.Load<CardSharedParams>("res://assets/cards/card_shared.tres"); }
-		else {
-			GD.PushWarning("Could not load shared card info.");
-			SharedParams = new();
-		}
 	}
+	
+	private static CardSharedParams SharedParams { get { if (!field.IsValid()) field = ResourceLoader.Load<CardSharedParams>("res://assets/cards/card_shared.tres") ?? new(); return field; } }
 
 	public CardDisplay() { MouseFilter = MouseFilterEnum.Pass; FocusMode = FocusModeEnum.All; }
 
