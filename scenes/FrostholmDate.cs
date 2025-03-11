56755f2e86991a298116f8b64fd257a70a66781c
Finn Else-McCormick <2200864@uad.ac.uk>
2025-03-11 19:00:31 +0000

    Rearrange UI scenes into less messy hierarchy, removed some old left over scenes

--- scenes/FrostholmDate.cs
+++ scenes/FrostholmDate.cs
@@ -5,7 +5,7 @@ namespace DamselsGambit;
 public partial class FrostholmDate : Date
 {
     public override void OnGameEnd() {
-        var endScreen = ResourceLoader.Load<PackedScene>("res://scenes/ui/end_screen.tscn").Instantiate<EndScreen>();
+        var endScreen = ResourceLoader.Load<PackedScene>("res://scenes/ui/menu/end_screen.tscn").Instantiate<EndScreen>();
 		AddChild(endScreen);
 		endScreen.MessageLabel.Text = _cardGameController.Score switch {
 			_ when _cardGameController.Score >= _cardGameController.LoveThreshold => "Prepare for marriage.",
