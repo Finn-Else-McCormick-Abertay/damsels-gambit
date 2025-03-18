fd9d8025b5a9fff71e2909293000e50a41a51921
h-wthorn <ellostirling@gmail.com>
2025-03-18 18:14:54 +0000

    focused states on most buttons bar the menu

--- scenes/dates/FrostholmDate.cs
+++ scenes/dates/FrostholmDate.cs
@@ -4,13 +4,13 @@ namespace DamselsGambit;
 
 public partial class FrostholmDate : BaseDate
 {
-    public override void OnGameEnd() {
-        var endScreen = ResourceLoader.Load<PackedScene>("res://scenes/ui/menu/end_screen.tscn").Instantiate<EndScreen>();
+	public override void OnGameEnd() {
+		var endScreen = ResourceLoader.Load<PackedScene>("res://scenes/ui/menu/end_screen.tscn").Instantiate<EndScreen>();
 		AddChild(endScreen);
 		endScreen.MessageLabel.Text = _cardGameController.Score switch {
 			_ when _cardGameController.Score >= _cardGameController.LoveThreshold => "Prepare for marriage.",
 			_ when _cardGameController.Score <= _cardGameController.HateThreshold => "Prepare for war.",
 			_ => "You win!"
 		};
-    }
+	}
 }
--- scenes/ui/menu/settings_menu/SettingsMenu.cs
+++ scenes/ui/menu/settings_menu/SettingsMenu.cs
@@ -6,31 +6,31 @@ namespace DamselsGambit;
 
 public partial class SettingsMenu : Control, IFocusContext, IBackContext
 {
-    [Export] private TabContainer TabContainer { get; set; }
-    [Export] private BaseButton ExitButton { get; set; }
+	[Export] private TabContainer TabContainer { get; set; }
+	[Export] private BaseButton ExitButton { get; set; }
 
-    public event Action OnExit;
+	public event Action OnExit;
 
-    public override void _EnterTree() {
-        ExitButton?.TryConnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnExitPressed));
-    }
-    public override void _ExitTree() {
-        ExitButton?.TryDisconnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnExitPressed));
-    }
+	public override void _EnterTree() {
+		ExitButton?.TryConnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnExitPressed));
+	}
+	public override void _ExitTree() {
+		ExitButton?.TryDisconnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnExitPressed));
+	}
 
-    private void OnExitPressed() => OnExit?.Invoke();
-    
-    public virtual int FocusContextPriority => 10;
+	private void OnExitPressed() => OnExit?.Invoke();
+	
+	public virtual int FocusContextPriority => 10;
 
-    public Control GetDefaultFocus() => TabContainer.GetTabBar();
-    public Control GetDefaultFocus(InputManager.FocusDirection direction) => direction switch {
-        InputManager.FocusDirection.Up => ExitButton,
-        _ => TabContainer.GetTabBar()
-    };
+	public Control GetDefaultFocus() => TabContainer.GetTabBar();
+	public Control GetDefaultFocus(InputManager.FocusDirection direction) => direction switch {
+		InputManager.FocusDirection.Up => ExitButton,
+		_ => TabContainer.GetTabBar()
+	};
 
-    public virtual int BackContextPriority => 10;
-    public bool UseBackInput() {
-        OnExit?.Invoke();
-        return true;
-    }
+	public virtual int BackContextPriority => 10;
+	public bool UseBackInput() {
+		OnExit?.Invoke();
+		return true;
+	}
 }
