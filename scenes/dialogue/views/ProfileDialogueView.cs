6be8ddf6d31c5486f810d9958116feea001b6b2f
h-wthorn <ellostirling@gmail.com>
2025-03-19 10:45:06 +0000

    fixed some things i messed up last night

--- scenes/dialogue/views/ProfileDialogueView.cs
+++ scenes/dialogue/views/ProfileDialogueView.cs
@@ -11,56 +11,56 @@ namespace DamselsGambit.Dialogue;
 
 public partial class ProfileDialogueView : Node, DialogueViewBase
 {
-    public string ProfileNode { get; set { field = value; Update(); } }
+	public string ProfileNode { get; set { field = value; Update(); } }
 
-    [Export] private Label Title { get; set; }
-    [Export] private RichTextLabel Subtitle { get; set; }
-    [Export] private RichTextLabel Label { get; set; }
-    [Export] private TextureRect Portrait { get; set; }
+	[Export] private Label Title { get; set; }
+	[Export] private RichTextLabel Subtitle { get; set; }
+	[Export] private RichTextLabel Label { get; set; }
+	[Export] private TextureRect Portrait { get; set; }
 
-    private StringBuilder _stringBuilder;
+	private StringBuilder _stringBuilder;
 
-    public override void _EnterTree() {
-        Title.Text = ""; Subtitle.Text = ""; Label.Text = ""; Portrait.Texture = null;
-        DialogueManager.Register(this);
-        DialogueManager.Knowledge.OnChanged += Update;
-        this.OnReady(Update);
-    }
-    public override void _ExitTree() {
-        DialogueManager.Deregister(this);
-        DialogueManager.Knowledge.OnChanged -= Update;
-    }
+	public override void _EnterTree() {
+		Title.Text = ""; Subtitle.Text = ""; Label.Text = ""; Portrait.Texture = null;
+		DialogueManager.Register(this);
+		DialogueManager.Knowledge.OnChanged += Update;
+		this.OnReady(Update);
+	}
+	public override void _ExitTree() {
+		DialogueManager.Deregister(this);
+		DialogueManager.Knowledge.OnChanged -= Update;
+	}
 
-    private void Update() {
-        if (!DialogueManager.ProfileRunner.IsValid() || string.IsNullOrEmpty(ProfileNode)) return;
-        if (DialogueManager.ProfileRunner.IsDialogueRunning) DialogueManager.ProfileRunner.Stop();
-        if (DialogueManager.DialogueExists(ProfileNode)) DialogueManager.ProfileRunner.StartDialogue(ProfileNode); else Console.Warning($"Profile view could not update: '{ProfileNode}' is not a valid dialogue node.");
-    }
+	private void Update() {
+		if (!DialogueManager.ProfileRunner.IsValid() || string.IsNullOrEmpty(ProfileNode)) return;
+		if (DialogueManager.ProfileRunner.IsDialogueRunning) DialogueManager.ProfileRunner.Stop();
+		if (DialogueManager.DialogueExists(ProfileNode)) DialogueManager.ProfileRunner.StartDialogue(ProfileNode); else Console.Warning($"Profile view could not update: '{ProfileNode}' is not a valid dialogue node.");
+	}
 
-    public Action requestInterrupt { get; set; }
+	public Action requestInterrupt { get; set; }
 
-    public void DialogueStarted() {
-        _stringBuilder = new();
-        var nodeTags = DialogueManager.ProfileRunner.GetTagsForNode(ProfileNode).Select(x => x.StripFront('#'));
-        var complexTags = nodeTags.Select(x => x.Split(':')).Where(x => x.Length == 2).Select(x => (x.First().ToLower(), x.Last())).ToDictionary();
+	public void DialogueStarted() {
+		_stringBuilder = new();
+		var nodeTags = DialogueManager.ProfileRunner.GetTagsForNode(ProfileNode).Select(x => x.StripFront('#'));
+		var complexTags = nodeTags.Select(x => x.Split(':')).Where(x => x.Length == 2).Select(x => (x.First().ToLower(), x.Last())).ToDictionary();
 
-        if (Portrait.IsValid() && complexTags.TryGetValue("portrait", out var path)) Portrait.Texture = ResourceLoader.Load<Texture2D>($"res://{path.Replace('\\','/').StripFront("res://")}");
-    }
+		if (Portrait.IsValid() && complexTags.TryGetValue("portrait", out var path)) Portrait.Texture = ResourceLoader.Load<Texture2D>($"res://{path.Replace('\\','/').StripFront("res://")}");
+	}
 
-    public void RunLine(LocalizedLine line, Action onLineFinished) {
-        bool HasTag(string tag) => line.Metadata?.Any(x => x.MatchN(tag)) ?? false;
+	public void RunLine(LocalizedLine line, Action onLineFinished) {
+		bool HasTag(string tag) => line.Metadata?.Any(x => x.MatchN(tag)) ?? false;
 
-        var lineText = line.Text.AsBBCode();
+		var lineText = line.Text.AsBBCode();
 
-        if (HasTag("title")) Title.Text = lineText;
-        else if (HasTag("subtitle")) Subtitle.Text = lineText;
-        else if (!string.IsNullOrWhiteSpace(lineText)) _stringBuilder.AppendLine(lineText);
+		if (HasTag("title")) Title.Text = lineText;
+		else if (HasTag("subtitle")) Subtitle.Text = lineText;
+		else if (!string.IsNullOrWhiteSpace(lineText)) _stringBuilder.AppendLine(lineText);
 
-        onLineFinished?.Invoke();
+		onLineFinished?.Invoke();
 	}
 
 	public void RunOptions(DialogueOption[] dialogueOptions, Action<int> onOptionSelected) => Console.Error("Profile view encountered dialogue options.");
-    
+	
 	public void DialogueComplete() { Label.Text = _stringBuilder?.ToString(); }
 
 }
--- scenes/game/suitor_profile/SuitorProfile.cs
+++ scenes/game/suitor_profile/SuitorProfile.cs
@@ -8,61 +8,61 @@ namespace DamselsGambit;
 [Tool]
 public partial class SuitorProfile : Control
 {
-    [Export] public string SuitorName { get; set { field = value; UpdateDialogueViewNode(); } }
+	[Export] public string SuitorName { get; set { field = value; UpdateDialogueViewNode(); } }
 
-    [ExportCategory("Offset")]
-    [Export] public Vector2 HighlightOffset { get; set; }
-    [Export] public Vector2 OpenOffset { get; set; }
+	[ExportCategory("Offset")]
+	[Export] public Vector2 HighlightOffset { get; set; }
+	[Export] public Vector2 OpenOffset { get; set; }
 
-    [Export] private double HighlightDuration { get; set; } = 0.2;
-    [Export] private double OpenDuration { get; set; } = 0.3;
+	[Export] private double HighlightDuration { get; set; } = 0.2;
+	[Export] private double OpenDuration { get; set; } = 0.3;
 
-    [ExportGroup("Nodes")]
-    [Export] private Node DialogueView { get; set { field = value; UpdateDialogueViewNode(); } }
-    [Export] private Control Root { get; set; }
-    [Export] private Button TabButton { get; set; }
+	[ExportGroup("Nodes")]
+	[Export] private Node DialogueView { get; set { field = value; UpdateDialogueViewNode(); } }
+	[Export] private Control Root { get; set; }
+	[Export] private Button TabButton { get; set; }
 
-    [ExportGroup("Debug", "Debug")]
-    [Export] private bool DebugOpen { get => Open; set => Open = value; }
-    [Export] private bool DebugHighlighted { get => Highlighted; set => Highlighted = value; }
-    
-    private Tween _moveTween;
-    public bool Open {
-        get; set {
-            if (Open != value || (_moveTween?.IsRunning() ?? false)) {
-                var offset = OpenOffset; if (Highlighted) { offset -= HighlightOffset; } offset *= value ? 1f : -1f;
-                this.OnReady(() => { _moveTween?.Kill(); _moveTween = Root.CreateTween(); _moveTween.TweenProperty(Root, "position", value ? OpenOffset : Highlighted ? HighlightOffset : new Vector2(), OpenDuration); });
-            }
-            field = value;
-        }
-    } = false;
-    public bool Highlighted {
-        get; set {
-            if ((Highlighted != value || (_moveTween?.IsRunning() ?? false)) && !Open) this.OnReady(() => {
-                _moveTween?.Kill(); _moveTween = Root.CreateTween(); _moveTween.TweenProperty(Root, "position", value ? HighlightOffset : new Vector2(), HighlightDuration);
-            });
-            field = value;
-        }
-    } = false;
+	[ExportGroup("Debug", "Debug")]
+	[Export] private bool DebugOpen { get => Open; set => Open = value; }
+	[Export] private bool DebugHighlighted { get => Highlighted; set => Highlighted = value; }
+	
+	private Tween _moveTween;
+	public bool Open {
+		get; set {
+			if (Open != value || (_moveTween?.IsRunning() ?? false)) {
+				var offset = OpenOffset; if (Highlighted) { offset -= HighlightOffset; } offset *= value ? 1f : -1f;
+				this.OnReady(() => { _moveTween?.Kill(); _moveTween = Root.CreateTween(); _moveTween.TweenProperty(Root, "position", value ? OpenOffset : Highlighted ? HighlightOffset : new Vector2(), OpenDuration); });
+			}
+			field = value;
+		}
+	} = false;
+	public bool Highlighted {
+		get; set {
+			if ((Highlighted != value || (_moveTween?.IsRunning() ?? false)) && !Open) this.OnReady(() => {
+				_moveTween?.Kill(); _moveTween = Root.CreateTween(); _moveTween.TweenProperty(Root, "position", value ? HighlightOffset : new Vector2(), HighlightDuration);
+			});
+			field = value;
+		}
+	} = false;
 
-    public override void _Ready() {
-        if (Engine.IsEditorHint()) return;
+	public override void _Ready() {
+		if (Engine.IsEditorHint()) return;
 
-        Open = false; Highlighted = false;
+		Open = false; Highlighted = false;
 
-        TabButton?.Connect(Control.SignalName.MouseEntered, new Callable(this, MethodName.TriggerHighlight));
-        TabButton?.Connect(Control.SignalName.MouseExited, new Callable(this, MethodName.TriggerUnhighlight));
-        TabButton?.Connect(Control.SignalName.FocusEntered, new Callable(this, MethodName.TriggerHighlight));
-        TabButton?.Connect(Control.SignalName.FocusExited, new Callable(this, MethodName.TriggerUnhighlight));
-        TabButton?.Connect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.ToggleOpen));
-    }
+		TabButton?.Connect(Control.SignalName.MouseEntered, new Callable(this, MethodName.TriggerHighlight));
+		TabButton?.Connect(Control.SignalName.MouseExited, new Callable(this, MethodName.TriggerUnhighlight));
+		TabButton?.Connect(Control.SignalName.FocusEntered, new Callable(this, MethodName.TriggerHighlight));
+		TabButton?.Connect(Control.SignalName.FocusExited, new Callable(this, MethodName.TriggerUnhighlight));
+		TabButton?.Connect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.ToggleOpen));
+	}
 
-    private void TriggerHighlight() { Highlighted = true; }
-    private void TriggerUnhighlight() { Highlighted = false; }
-    private void ToggleOpen() { Open = !Open; }
+	private void TriggerHighlight() { Highlighted = true; }
+	private void TriggerUnhighlight() { Highlighted = false; }
+	private void ToggleOpen() { Open = !Open; }
 
-    public void UpdateDialogueViewNode() {
-        if (Engine.IsEditorHint() || !DialogueView.IsValid() || string.IsNullOrEmpty(SuitorName)) return;
-        (DialogueView as ProfileDialogueView).ProfileNode = $"{Case.ToSnake(SuitorName)}__profile";
-    }
+	public void UpdateDialogueViewNode() {
+		if (Engine.IsEditorHint() || !DialogueView.IsValid() || string.IsNullOrEmpty(SuitorName)) return;
+		(DialogueView as ProfileDialogueView).ProfileNode = $"{Case.ToSnake(SuitorName)}__profile";
+	}
 }
