e6f7c865da1dd022b92a840a7558a742af20e62f
Finn Else-McCormick <2200864@uad.ac.uk>
2025-03-27 17:34:03 +0000

    Clean up ending logic by having the possible ending triggers all call an AttemptGameEnd method, which triggers the game end if the conditions are met. Also fix ending overwriting in-progress dialogue by having the ending be deferred until round if triggered mid right, eg by a score change. Also export variable from CardGameController to set whether to auto-fail on hitting the end of the bar, and still visually cap the score if not.

--- scenes/game/CardGameController.cs
+++ scenes/game/CardGameController.cs
@@ -11,24 +11,34 @@ using YarnSpinnerGodot;
 
 namespace DamselsGambit;
 
+public enum AffectionState { Neutral, Love, Hate }
+
 [Tool]
 public partial class CardGameController : Control, IReloadableToolScript, IFocusContext
 {
-	// Signals which are emitted on start and end of game and round respectively
-	[Signal] public delegate void GameStartEventHandler();
-	[Signal] public delegate void GameEndEventHandler();
-	[Signal] public delegate void RoundStartEventHandler(int round);
-	[Signal] public delegate void RoundEndEventHandler(int round);
+	// Signals which are emitted on start and end of each phase
+	[Signal] public delegate void IntroStartEventHandler(); 			[Signal] public delegate void IntroEndEventHandler();
+	[Signal] public delegate void GameStartEventHandler(); 				[Signal] public delegate void GameEndEventHandler();
+	[Signal] public delegate void RoundStartEventHandler(int round); 	[Signal] public delegate void RoundEndEventHandler(int round);
+	
+	public bool Started { get; private set; } = false;
+	public bool Ended { get; private set; } = false;
+	public bool MidRound { get; private set; } = false;
 	
 	// Current round. Triggers game end when it exceeds NumRounds
-	public int Round { get; set { field = value;  RoundMeter?.OnReady(x => x.CurrentRound = Round); if (Round > NumRounds) TriggerGameEnd(); } }
+	public int Round { get; set { field = value; RoundMeter?.OnReady(x => x.CurrentRound = Round); AttemptGameEnd(); } }
+	// Current score. When AutoFailOnHitThreshold is true, triggers game end when it exceeds score minimum or maximum
+	public int Score { get; set { field = value; AffectionMeter?.CreateTween()?.TweenProperty(AffectionMeter, AffectionMeter.PropertyName.Value, int.Clamp(Score, ScoreMin, ScoreMax), 1.0); AttemptGameEnd(); } }
 
-	// Current score. Automatically tweens the displayed value of the AffectionMeter to itself whenever set
-	public int Score { get; set { field = value; AffectionMeter?.CreateTween()?.TweenProperty(AffectionMeter, AffectionMeter.PropertyName.Value.ToString(), Score, 1.0); if (Score <= ScoreMin || Score >= ScoreMax) CallableUtils.CallDeferred(TriggerGameEnd); } }
+	// Current affection state based on score and thresholds
+	public AffectionState AffectionState => Score switch { _ when Score >= LoveThreshold => AffectionState.Love, _ when Score <= HateThreshold => AffectionState.Hate, _ => AffectionState.Neutral };
 
 	// Name of suitor. This converted to snake case will be used for generating dialogue node names, and for updating the profile
 	[Export] public string SuitorName { get; set { field = value; _suitorId = Case.ToSnake(SuitorName); GameManager.NotebookMenu?.OnReady(x => x.SuitorName = SuitorName); } }
 	private string _suitorId;
+	
+	// Should the skip intro button display? Set based on tags of intro node (cause it looks weird during an intro that's just a very short fade in)
+	public bool IntroSkippable { get; private set; } = false;
 
 	[Export(PropertyHint.Range, "0,20,")] public int NumRounds { get; private set { field = value; RoundMeter?.OnReady(() => RoundMeter.NumRounds = NumRounds); } } = 8;
 	
@@ -38,6 +48,8 @@ public partial class CardGameController : Control, IReloadableToolScript, IFocus
 
 	[Export(PropertyHint.Range, "-30,30,")] public int LoveThreshold { get; private set { field = value; AffectionMeter?.OnReady(x => x.LoveThreshold = LoveThreshold); } } = 4;
 	[Export(PropertyHint.Range, "-30,30,")] public int HateThreshold { get; private set { field = value; AffectionMeter?.OnReady(x => x.HateThreshold = HateThreshold); } } = -4;
+
+	[Export] public bool AutoFailOnHitThreshold { get; set; } = true;
 
 	[ExportGroup("Deck")]
 	[Export] public Godot.Collections.Dictionary<string, int> TopicDeck { get; set { field = value; this.OnReady(EditorUpdateHands); } }
@@ -74,14 +86,8 @@ public partial class CardGameController : Control, IReloadableToolScript, IFocus
 		Hide(); Round = 1;
 	}
 
-	// Has intro ended and game started? Tracked for skip logic
-	public bool Started { get; private set; } = false;
-
-	// Should the skip intro button display? Set based on tags of intro node (cause it looks weird during an intro that's just a very short fade in)
-	public bool IntroSkippable { get; private set; } = false;
-
 	// Trigger the start of the game
-	// This doens't happen in ready so that GameManager is able to set whether the intro should be skipped (which it is when loading into the scene directly)
+	// This doesn't happen in ready so that GameManager is able to set whether the intro should be skipped (which it is when loading into the scene directly)
 	public void BeginGame(bool skipIntro = false) {
 		// Creating list of cards from exported dictionary of cards and their counts
 		static List<string> CreateWorkingDeck(Godot.Collections.Dictionary<string, int> deck) { List<string> working = []; foreach (var (card, count) in deck) for (int i = 0; i < count; ++i) working.Add(card); return working; }
@@ -112,25 +118,25 @@ public partial class CardGameController : Control, IReloadableToolScript, IFocus
 		DialogueManager.TryRun(skipIntro ? $"{_suitorId}__skip_setup" : $"{_suitorId}__intro")
 			.AndThen(() => {
 				Show(); GameManager.NotebookMenu.Show();
-				Started = true;
-				Round = 1; Deal();
-				EmitSignal(SignalName.RoundStart, Round);
+				Started = true; Round = 1;
+				AttemptStartRound();
 			});
 	}
 
 	public override void _ExitTree() {
+		if (Engine.IsEditorHint()) return;
 		GameManager.NotebookMenu.FocusNeighborBottom = new();
 		GameManager.NotebookMenu.FocusNeighborLeft = new();
 	}
 
 	// If not yet started, force-run the skip_setup node, skipping to the start of gameplay even if currently in the intro
-	public void ForceSkipIntro() { if (!Started) DialogueManager.Run($"{_suitorId}__skip_setup", true); }
+	public void ForceSkipIntro() { if (!Started && !Ended) DialogueManager.Run($"{_suitorId}__skip_setup", true); }
 
 	public override void _Process(double delta) {
 		if (Engine.IsEditorHint()) return;
 
 		// Set play button to be disabled based on whether the correct number of cards are selected
-		PlayButton.Disabled = TopicHand.GetSelected().Count() != 1 || ActionHand.GetSelected().Count() != 1;
+		PlayButton.Disabled = TopicHand.GetSelected().Count() != 1 || ActionHand.GetSelected().Count() != 1 || ShouldGameEnd();
 	}
 	
 	public Control GetDefaultFocus(FocusDirection direction) =>
@@ -144,6 +150,8 @@ public partial class CardGameController : Control, IReloadableToolScript, IFocus
 	// Connected to PlayButton's Pressed signal
 	private void PlayHand() {
 		if (Engine.IsEditorHint()) return;
+		if (!Started) { Console.Warning("Attempted to play hand before game start."); return; }
+		if (Ended) { Console.Warning("Attempted to play hand after game end."); return; }
 
 		CardDisplay selectedTopic = TopicHand.GetSelected().SingleOrDefault();
 		CardDisplay selectedAction = ActionHand.GetSelected().SingleOrDefault();
@@ -161,28 +169,45 @@ public partial class CardGameController : Control, IReloadableToolScript, IFocus
 		// Then, emit round signal, increment round, and handle next round start
 		DialogueManager.Run(dialogueNode)
 			.AndThen(() => {
+				MidRound = false;
 				EmitSignal(SignalName.RoundEnd, Round);
-				Round += 1;
-				if (Round <= NumRounds) { PlayButton.Show(); Deal(); EmitSignal(SignalName.RoundStart, Round); }
+				++Round; AttemptStartRound();
 			});
 	}
 
-	// Called by the Round setter
-	private void TriggerGameEnd() {
-		if (Engine.IsEditorHint()) return;
+	// Are preconditions for game end met
+	private bool ShouldGameEnd() => Started && !Ended && (Round > NumRounds || (AutoFailOnHitThreshold && !RangeOf<int>.Between(ScoreMin, ScoreMax).Contains(Score)));
+
+	// Triggers round start so long as end preconditions are not met. Called by BeginGame and PlayHand
+	private void AttemptStartRound() {
+		if (MidRound) { Console.Warning("Failed to start round: previous round did not end."); return; }
+		if (Engine.IsEditorHint() || ShouldGameEnd()) return;
+
+		PlayButton.Show(); Deal();
+		MidRound = true;
+		EmitSignal(SignalName.RoundStart, Round);
+	}
+
+	// Triggers game end if the preconditions are met. Called by the Round and Score setters.
+	private void AttemptGameEnd() {
+		if (Engine.IsEditorHint() || !ShouldGameEnd()) return;
+
+		// If mid-round, defer ending until end of round
+		if (MidRound && Connect(SignalName.RoundEnd, Callable.From((int round) => AttemptGameEnd()), (uint)ConnectFlags.OneShot) == Error.Ok) return;
 
 		PlayButton.Hide();
 
-		// Trigger the pre ending node if it exists (the tutorial uses this to run the profile explanation, so the UI can't be hidden yet).
-		// Once it is finished (or if it didn't exist) then hide the UI and trigger the correct ending dialogue.
-		// Once that is finished, emit the game end signal.
-		DialogueManager.TryRun($"{_suitorId}__pre_ending")
-			.AndThen(() => {
-				AffectionMeter.Hide(); RoundMeter.Hide(); TopicHand.Hide(); ActionHand.Hide(); PlayButton.Hide();
-				DialogueManager
-					.TryRun($"{_suitorId}__ending__{Score switch { _ when Score >= LoveThreshold => "love", _ when Score <= HateThreshold => "hate", _ => "neutral" }}")
-					.AndThen(() => EmitSignal(SignalName.GameEnd));
-			});
+		CallableUtils.CallDeferred(() => {
+			// Trigger the pre ending node if it exists (the tutorial uses this to run the profile explanation, so the UI can't be hidden yet).
+			// Once it is finished (or if it didn't exist) then hide the UI and trigger the correct ending dialogue.
+			// Once that is finished, emit the game end signal.
+			DialogueManager.TryRun($"{_suitorId}__pre_ending")
+				.AndThen(() => {
+					AffectionMeter.Hide(); RoundMeter.Hide(); TopicHand.Hide(); ActionHand.Hide(); PlayButton.Hide();
+					DialogueManager.TryRun($"{_suitorId}__ending__{AffectionState switch { AffectionState.Love => "love", AffectionState.Hate => "hate", AffectionState.Neutral => "neutral"}}")
+						.AndThen(() => EmitSignal(SignalName.GameEnd));
+				});
+		});
 	}
 
 	// Deal up to each HandContainer's hand size, drawing from the working decks. Automatically handles tweens to animate them flying in from offscreen.
@@ -208,7 +233,7 @@ public partial class CardGameController : Control, IReloadableToolScript, IFocus
 		DealToHand(TopicHand, TopicHandSize, _topicDeckWorking, new Vector2(500f, 100f), 30f);
 	}
 
-	// Equivalent of Deal, but deterministic and instant, for display in the editor
+	// Equivalent of Deal, but deterministic and instant (for display in the editor)
 	private void EditorUpdateHands() {
 		if (!Engine.IsEditorHint()) return;
 
