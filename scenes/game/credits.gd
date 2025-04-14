07f93bab68feb74a73248da010cffb4c2335f8c6
h-wthorn <ellostirling@gmail.com>
2025-04-14 17:29:41 +0100

    Fixed Credits in Master

--- scenes/game/credits.gd
+++ scenes/game/credits.gd
@@ -3,7 +3,10 @@ extends Control
 @export var scroll_speed : float = 5.0
 @onready var text = $CreditsText
 
-func _process(_delta: float) -> void:
+
+
+
+func _process(delta: float) -> void:
 	#text.position -= Vector2(0.0, scroll_speed*delta)
 	if (text.position.y + text.size.y > -100):
 		text.position = lerp(text.position, text.position-Vector2(0.0, scroll_speed), 0.1)
