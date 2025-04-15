4124802aec90191e885f4965b1747cc365e47851
Finn Else-McCormick <2200864@uad.ac.uk>
2025-04-15 15:28:45 +0100

    Have the skip button hidden until recieving an input event

--- scenes/game/credits.gd
+++ scenes/game/credits.gd
@@ -6,11 +6,19 @@ extends Control
 
 var credits_tween : Tween
 
-func _ready():
+func _ready() -> void:
+	# Start out skip button as hidden
+	skip_button.hide()
 	skip_button.button_down.connect(on_credits_end)
+
 	credits_tween = credits_text.create_tween()
 	credits_tween.tween_property(credits_text, "position:y", -(credits_text.size.y + credits_text.position.y), 20)
 	credits_tween.tween_callback(on_credits_end)
+
+func _input(event: InputEvent) -> void:
+	# Display skip button if input recieved
+	if event is not InputEventMouseMotion && event is not InputEventScreenDrag:
+		skip_button.show()
 
 func on_credits_end() -> void:
 	GameManager.SwitchToMainMenu()
