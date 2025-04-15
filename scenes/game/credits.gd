2479f7c642c8db8db04bb6e40950226d4866bcd0
Finn Else-McCormick <2200864@uad.ac.uk>
2025-04-15 15:24:34 +0100

    Have the credits automatically send you to the title screen after reaching the end

--- scenes/game/credits.gd
+++ scenes/game/credits.gd
@@ -10,6 +10,7 @@ func _ready():
 	skip_button.button_down.connect(on_credits_end)
 	credits_tween = credits_text.create_tween()
 	credits_tween.tween_property(credits_text, "position:y", -(credits_text.size.y + credits_text.position.y), 20)
+	credits_tween.tween_callback(on_credits_end)
 
 func on_credits_end() -> void:
 	GameManager.SwitchToMainMenu()
