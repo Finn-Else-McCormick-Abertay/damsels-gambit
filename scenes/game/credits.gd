38313343620397c5c8030c571d21656075d0026f
h-wthorn <ellostirling@gmail.com>
2025-04-15 11:05:28 +0100

    added link to the credits from the end screen

--- scenes/game/credits.gd
+++ scenes/game/credits.gd
@@ -2,7 +2,7 @@ extends Control
 
 @export var scroll_speed : float = 5.0
 @onready var text = $CreditsText
-
+@onready var exit_button = $Exit_Button as Button
 
 
 
@@ -10,3 +10,12 @@ func _process(delta: float) -> void:
 	#text.position -= Vector2(0.0, scroll_speed*delta)
 	if (text.position.y + text.size.y > -100):
 		text.position = lerp(text.position, text.position-Vector2(0.0, scroll_speed), 0.1)
+
+
+signal exit_credits
+
+func _ready():
+	exit_button.button_down.connect(on_exit_pressed)
+
+func on_exit_pressed() -> void:
+	get_tree().quit()
--- scenes/menu/main/MainMenu.cs
+++ scenes/menu/main/MainMenu.cs
@@ -5,63 +5,63 @@ namespace DamselsGambit;
 
 public partial class MainMenu : Control, IFocusContext
 {
-    [Export] private BaseButton StartButton { get; set; }
-    [Export] private BaseButton SettingsButton { get; set; }
-    [Export] private BaseButton ExitButton { get; set; }
-    [Export] private Control ButtonRoot { get; set; }
+	[Export] private BaseButton StartButton { get; set; }
+	[Export] private BaseButton SettingsButton { get; set; }
+	[Export] private BaseButton ExitButton { get; set; }
+	[Export] private Control ButtonRoot { get; set; }
 
-    [Export] private PackedScene SettingsMenuScene { get; set; }
+	[Export] private PackedScene SettingsMenuScene { get; set; }
 
-    private SettingsMenu _settingsMenu;
+	private SettingsMenu _settingsMenu;
 
-    public override void _EnterTree() {
-        StartButton?.TryConnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnStartPressed));
-        SettingsButton?.TryConnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnSettingsPressed));
-        ExitButton?.TryConnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnExitPressed));
-    }
-    public override void _ExitTree() {
-        StartButton?.TryDisconnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnStartPressed));
-        SettingsButton?.TryDisconnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnSettingsPressed));
-        ExitButton?.TryDisconnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnExitPressed));
-    }
+	public override void _EnterTree() {
+		StartButton?.TryConnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnStartPressed));
+		SettingsButton?.TryConnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnSettingsPressed));
+		ExitButton?.TryConnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnExitPressed));
+	}
+	public override void _ExitTree() {
+		StartButton?.TryDisconnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnStartPressed));
+		SettingsButton?.TryDisconnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnSettingsPressed));
+		ExitButton?.TryDisconnect(BaseButton.SignalName.Pressed, new Callable(this, MethodName.OnExitPressed));
+	}
 
-    private void OnStartPressed() => GameManager.BeginGame();
+	private void OnStartPressed() => GameManager.BeginGame();
 
-    private void OnSettingsPressed() {
-	    InputManager.PushToFocusStack();
+	private void OnSettingsPressed() {
+		InputManager.PushToFocusStack();
 
-        if (_settingsMenu.IsValid()) {
-		    _settingsMenu.QueueFree();
-		    _settingsMenu = null;
-        }
+		if (_settingsMenu.IsValid()) {
+			_settingsMenu.QueueFree();
+			_settingsMenu = null;
+		}
 
-        _settingsMenu = SettingsMenuScene?.Instantiate<SettingsMenu>();
-        AddChild(_settingsMenu);
+		_settingsMenu = SettingsMenuScene?.Instantiate<SettingsMenu>();
+		AddChild(_settingsMenu);
 
-        _settingsMenu.OnExit += OnExitSettingsMenu;
-        
-        ButtonRoot.Hide();
-    }
+		_settingsMenu.OnExit += OnExitSettingsMenu;
+		
+		ButtonRoot.Hide();
+	}
 
-    private void OnExitSettingsMenu() {
-        _settingsMenu.OnExit -= OnExitSettingsMenu;
-        
-        ButtonRoot.Show();
-        
-        _settingsMenu.Hide();
-        _settingsMenu.QueueFree();
-        _settingsMenu = null;
-        
-	    InputManager.PopFromFocusStack();
-    }
+	private void OnExitSettingsMenu() {
+		_settingsMenu.OnExit -= OnExitSettingsMenu;
+		
+		ButtonRoot.Show();
+		
+		_settingsMenu.Hide();
+		_settingsMenu.QueueFree();
+		_settingsMenu = null;
+		
+		InputManager.PopFromFocusStack();
+	}
 
-    private void OnExitPressed() => GetTree().Quit();
-    
-    public virtual int FocusContextPriority => 8;
+	private void OnExitPressed() => GetTree().Quit();
+	
+	public virtual int FocusContextPriority => 8;
 
-    public Control GetDefaultFocus() => StartButton;
-    public Control GetDefaultFocus(FocusDirection direction) => direction switch {
-        FocusDirection.Left => ExitButton,
-        _ => StartButton
-    };
+	public Control GetDefaultFocus() => StartButton;
+	public Control GetDefaultFocus(FocusDirection direction) => direction switch {
+		FocusDirection.Left => ExitButton,
+		_ => StartButton
+	};
 }
