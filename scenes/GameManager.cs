8abf026234b5d75fad1213bfb669e40b73186c1f
Finn Else-McCormick <2200864@uad.ac.uk>
2025-04-16 11:11:29 +0100

    Templated tween extensions using [MustBeVariant]

--- scenes/GameManager.cs
+++ scenes/GameManager.cs
@@ -154,9 +154,9 @@ public sealed partial class GameManager : Node
 	public static void Quit() {
 		float tweenDuration = 1.5f;
 
-		var masterBusIndex = AudioServer.GetBusIndex("Master");
+		var masterIdx = AudioServer.GetBusIndex("Master");
 		var volumeTween = Instance.CreateTween();
-		volumeTween.TweenMethod(x => AudioServer.SetBusVolumeLinear(masterBusIndex, (float)x.AsDouble()), AudioServer.GetBusVolumeLinear(masterBusIndex), 0, tweenDuration);
+		volumeTween.TweenMethod(volume => AudioServer.SetBusVolumeLinear(masterIdx, volume), AudioServer.GetBusVolumeLinear(masterIdx), 0, tweenDuration);
 
 		SceneTransition.Run(SceneTransition.Type.FadeToBlack, tweenDuration * 2, () => Instance.GetTree().Quit());
 	}
--- util/extensions/TweenExtensions.cs
+++ util/extensions/TweenExtensions.cs
@@ -5,14 +5,21 @@ namespace DamselsGambit.Util;
 
 public static class TweenExtensions
 {
-    // Just a helper to allow you to pass a StringName as the property path without having to call ToString on it (since nodes expose their property names as StringNames, not NodePaths)
+    /// <inheritdoc cref="Tween.TweenProperty(GodotObject, NodePath, Variant, double)"/>
+    public static PropertyTweener TweenProperty<[MustBeVariant]T>(this Tween self, GodotObject @object, string property, T finalVal, double duration) => self.TweenProperty(@object, property, Variant.From(finalVal), duration);
 
     /// <inheritdoc cref="Tween.TweenProperty(GodotObject, NodePath, Variant, double)"/>
     public static PropertyTweener TweenProperty(this Tween self, GodotObject @object, StringName property, Variant finalVal, double duration) => self.TweenProperty(@object, property.ToString(), finalVal, duration);
+
+    /// <inheritdoc cref="Tween.TweenProperty(GodotObject, NodePath, Variant, double)"/>
+    public static PropertyTweener TweenProperty<[MustBeVariant]T>(this Tween self, GodotObject @object, StringName property, T finalVal, double duration) => self.TweenProperty(@object, property, Variant.From(finalVal), duration);
 
     /// <inheritdoc cref="Tween.TweenCallback(Callable)"/>
     public static CallbackTweener TweenCallback(this Tween self, Action callback) => self.TweenCallback(Callable.From(callback));
     
     /// <inheritdoc cref="Tween.TweenMethod(Callable, Variant, Variant, double)"/>
     public static MethodTweener TweenMethod(this Tween self, Action<Variant> callback, Variant from, Variant to, double duration) => self.TweenMethod(Callable.From(callback), from, to, duration);
+
+    /// <inheritdoc cref="Tween.TweenMethod(Callable, Variant, Variant, double)"/>
+    public static MethodTweener TweenMethod<[MustBeVariant]T>(this Tween self, Action<T> callback, T from, T to, double duration) => self.TweenMethod(Callable.From((Variant x) => callback(x.As<T>())), Variant.From(from), Variant.From(to), duration);
 }
