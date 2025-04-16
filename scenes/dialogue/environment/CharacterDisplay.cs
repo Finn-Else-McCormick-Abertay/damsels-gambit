d0e8ee9a338b34d13df65d171771d9183bc3ed01
Finn Else-McCormick <2200864@uad.ac.uk>
2025-04-16 12:56:00 +0100

    Improve prop animate dialogue command, add proper console warnings for incorrect or failed usage of dialogue commands

--- scenes/dialogue/environment/CharacterDisplay.cs
+++ scenes/dialogue/environment/CharacterDisplay.cs
@@ -9,6 +9,8 @@ public partial class CharacterDisplay : Sprite2D, IEnvironmentDisplay
 	[Export] public StringName CharacterName { get; set { field = value; TextureRoot = $"res://assets/characters/{CharacterName}/"; } }	
 	[Export] public string SpriteName { get; set { field = value; TexturePath = TextureRoot + SpriteName + ".png"; } }
 
+	public bool SpriteExists => Texture != null;
+
 	private string TextureRoot { get; set { field = value; TexturePath = TextureRoot + SpriteName + ".png"; } }
 	private string TexturePath { get; set { field = value; Texture = ResourceLoader.Exists(TexturePath) ? ResourceLoader.Load<Texture2D>(TexturePath) : null; } }
 
