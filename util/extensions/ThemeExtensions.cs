78b6e7fef3ab3a28516fa550c00a370b8bbb5032
Finn Else-McCormick <2200864@uad.ac.uk>
2025-04-22 19:05:34 +0100

    Improve affection meter animation, for some reason

--- util/extensions/ThemeExtensions.cs
+++ util/extensions/ThemeExtensions.cs
@@ -6,10 +6,24 @@ namespace DamselsGambit.Util;
 
 public static class ThemeExtensions
 {
-    public static Color? TryGetColor(this Theme self, StringName property, StringName themeType) => self.HasColor(property, themeType) ? self.GetColor(property, themeType) : null;
-    public static int? TryGetConstant(this Theme self, StringName property, StringName themeType) => self.HasConstant(property, themeType) ? self.GetConstant(property, themeType) : null;
-    public static Font? TryGetFont(this Theme self, StringName property, StringName themeType) => self.HasFont(property, themeType) ? self.GetFont(property, themeType) : null;
-    public static int? TryGetFontSize(this Theme self, StringName property, StringName themeType) => self.HasFontSize(property, themeType) ? self.GetFontSize(property, themeType) : null;
-    public static StyleBox? TryGetStylebox(this Theme self, StringName property, StringName themeType) => self.HasStylebox(property, themeType) ? self.GetStylebox(property, themeType) : null;
-    public static Texture2D? TryGetIcon(this Theme self, StringName property, StringName themeType) => self.HasIcon(property, themeType) ? self.GetIcon(property, themeType) : null;
+    public static bool TryGetColor(this Theme self, StringName property, StringName themeType, out Color color) { color = self.GetColor(property, themeType); return self.HasColor(property, themeType); }
+    public static bool TryGetConstant(this Theme self, StringName property, StringName themeType, out int constant) { constant = self.GetConstant(property, themeType); return self.HasConstant(property, themeType); }
+    public static bool TryGetFont(this Theme self, StringName property, StringName themeType, out Font font) { font = self.GetFont(property, themeType); return self.HasFont(property, themeType); }
+    public static bool TryGetFontSize(this Theme self, StringName property, StringName themeType, out int fontSize) { fontSize = self.GetFontSize(property, themeType); return self.HasFontSize(property, themeType); }
+    public static bool TryGetStylebox(this Theme self, StringName property, StringName themeType, out StyleBox stylebox) { stylebox = self.GetStylebox(property, themeType); return self.HasStylebox(property, themeType); }
+    public static bool TryGetIcon(this Theme self, StringName property, StringName themeType, out Texture2D icon) { icon = self.GetIcon(property, themeType); return self.HasIcon(property, themeType); }
+
+    public static Color GetColorOr(this Theme self, StringName property, StringName themeType, Color fallback) => self.HasColor(property, themeType) ? self.GetColor(property, themeType) : fallback;
+    public static int GetConstantOr(this Theme self, StringName property, StringName themeType, int fallback) => self.HasConstant(property, themeType) ? self.GetConstant(property, themeType) : fallback;
+    public static Font GetFontOr(this Theme self, StringName property, StringName themeType, Font fallback) => self.HasFont(property, themeType) ? self.GetFont(property, themeType) : fallback;
+    public static int GetFontSizeOr(this Theme self, StringName property, StringName themeType, int fallback) => self.HasFontSize(property, themeType) ? self.GetFontSize(property, themeType) : fallback;
+    public static StyleBox GetStyleboxOr(this Theme self, StringName property, StringName themeType, StyleBox fallback) => self.HasStylebox(property, themeType) ? self.GetStylebox(property, themeType) : fallback;
+    public static Texture2D GetIconOr(this Theme self, StringName property, StringName themeType, Texture2D fallback) => self.HasIcon(property, themeType) ? self.GetIcon(property, themeType) : fallback;
+
+    public static Color? GetColorOrDefault(this Theme self, StringName property, StringName themeType) => self.HasColor(property, themeType) ? self.GetColor(property, themeType) : default;
+    public static int? GetConstantOrDefault(this Theme self, StringName property, StringName themeType) => self.HasConstant(property, themeType) ? self.GetConstant(property, themeType) : default;
+    public static Font? GetFontOrDefault(this Theme self, StringName property, StringName themeType) => self.HasFont(property, themeType) ? self.GetFont(property, themeType) : default;
+    public static int? GetFontSizeOrDefault(this Theme self, StringName property, StringName themeType) => self.HasFontSize(property, themeType) ? self.GetFontSize(property, themeType) : default;
+    public static StyleBox? GetStyleboxOrDefault(this Theme self, StringName property, StringName themeType) => self.HasStylebox(property, themeType) ? self.GetStylebox(property, themeType) : default;
+    public static Texture2D? GetIconOrDefault(this Theme self, StringName property, StringName themeType) => self.HasIcon(property, themeType) ? self.GetIcon(property, themeType) : default;
 }
