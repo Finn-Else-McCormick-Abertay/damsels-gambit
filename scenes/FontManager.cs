17572551e8b7716459cd939311bcd3241a1f26d0
h-wthorn <ellostirling@gmail.com>
2025-04-26 10:18:59 +0100

    Added new font to fix issues

--- scenes/FontManager.cs
+++ scenes/FontManager.cs
@@ -8,7 +8,7 @@ public static class FontManager
 	{
 		public static readonly Font ArgosGeorge = ResourceLoader.Load<Font>("res://assets/fonts/argos-george/ArgosGeorge.ttf");
 		public static readonly Font Vinque = ResourceLoader.Load<Font>("res://assets/fonts/vinque/Vinque Rg.otf");
-		public static readonly Font OpenDyslexic = ResourceLoader.Load<Font>("res://assets/fonts/open-dyslexic/OpenDyslexic3-Regular.ttf");
+		public static readonly Font OpenDyslexic = ResourceLoader.Load<Font>("res://assets/fonts/open-dyslexic/OpenDyslexicEditedForDamsel'sGambit.otf");
 	}
 	
 	private static readonly FontVariation _mainDynamicFont = ResourceLoader.Load<FontVariation>("res://assets/fonts/main_ui_dynamic_font.tres");
