a0bc6332ba7790215ccb03312e2894c2f9d7e976
Finn Else-McCormick <2200864@uad.ac.uk>
2025-04-28 14:10:19 +0100

    Create bash script for exporting git history as correctly formatted csv

---
+++ logs/script/generate_log.ps1
@@ -0,0 +1,16 @@
+# generate_log.ps1
+
+# Generate log in csv format
+git log --pretty=format:"%h,%an,%ah,&!&%(decorate)&!&,%x22%s%x22" | Tee-Object -Variable log | Out-Null
+
+# Clean up usernames
+$log = $log -replace "beaparsons", "Bea Parsons"
+$log = $log -replace "h-wthorn|THORN STIRLING", "Thorn Stirling"
+$log = $log -replace "Finn-Else-McCormick-Abertay", "Finn Else-McCormick"
+
+# Reformat decorate step
+$log = $log -replace '&!&\s?(?:(?:tag: (?<tag>[^,\)]*))|.)*&!&', '${tag}'
+
+# Save to file
+$date = Get-Date -Format "dd/MM/yyyy HH-mm-ss"
+$log | Out-File -FilePath "${(Get-Item .).FullName}generated-logs/Commit History $date.csv" -Encoding ascii -Force
